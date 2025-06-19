aa4a9ce4ecfface2fa34a86ab8aeccb478253160
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-19 16:25:08 +0530

    Implemented the start of main.go (consistent with backend code structure)

--- cmd/aclapi/main.go
+++ cmd/aclapi/main.go
@@ -1,1 +1,40 @@
 package aclapi
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	if err := exec(); err != nil {
+		os.Exit(1)	
+	}
+}
+
+func exec() error {
+	
+	/* config must load here in exec() if needed to */
+
+	utils.InitLogger(true)
+
+	/* preparing graceful shutdown */
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-interrupt
+		cancel()
+	}()
+
+	return run(ctx)
+}
+
+func run(ctx context.Context) error {
+		
+
+}
