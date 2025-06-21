f110f8940198e4be726a1963f256e0028bf30cb8
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-22 03:13:58 +0530

    Added comments to handlers

--- internal/grpcserver/handlers.go
+++ internal/grpcserver/handlers.go
@@ -6,10 +6,12 @@ import (
 	pb "github.com/PythonHacker24/linux-acl-management-aclapi/internal/grpcserver/protos"
 )
 
+/* ping handler for gRPC (testing purposes only) */
 type PingHandler struct {
 	pb.UnimplementedPingServiceServer
 }
 
+/* defination for ping handler */
 func (h *PingHandler) Ping(ctx context.Context, req *pb.PingRequest) (*pb.PingResponse, error) {
 	return &pb.PingResponse{Message: "pong from module"}, nil
 }
