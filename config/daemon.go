2498a553c6ac75b05f0a459faadb06b320dfb0c7
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-23 16:06:40 +0530

    Updated config for considering Unix socket path for core daemon communication (IPC)

--- config/daemon.go
+++ config/daemon.go
@@ -2,7 +2,8 @@ package config
 
 /* daemon config */
 type DConfig struct {
-	DebugMode 	bool  `yaml:"debug_mode,omitempty"`   
+	DebugMode 	bool		`yaml:"debug_mode,omitempty"`   
+	SocketPath  string		`yaml:"socket_path,omitempty"`
 }
 
 /* normalization function */
@@ -12,6 +13,11 @@ func (d *DConfig) Normalize() error {
 		debug_mode is false by default
 		daemon will run on production mode by default
 	*/
+
+	/* if SocketPath is empty, use "/var/run/laclm-daemon.sock" as default */
+	if d.SocketPath == "" {
+		d.SocketPath = "/var/run/laclm-daemon.sock"
+	}
 
 	return nil
 }
