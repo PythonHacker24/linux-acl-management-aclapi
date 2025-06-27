45b0cae0b3f97a654f65dbfc58a1ae5c1b431c46
PythonHacker24 <adityapatil24680@gmail.com>
2025-06-28 02:25:10 +0530

    Added Mutual TLS loader

--- internal/grpcserver/utils.go
+++ internal/grpcserver/utils.go
@@ -2,19 +2,40 @@ package grpcserver
 
 import (
 	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"os"
 
 	"google.golang.org/grpc/credentials"
 )
 
 /* load tls config for gRPC server */
-func loadTLSCredentials(TLSCertFile, TLSKeyFile string) (credentials.TransportCredentials, error) {
+func loadMutualTLSCredentials(TLSCertFile, TLSKeyFile, TLSCACertFile string) (credentials.TransportCredentials, error) {
+	/* load TLS certificate and keys */
 	cert, err := tls.LoadX509KeyPair(TLSCertFile, TLSKeyFile)
 	if err != nil {
 		return nil, err
 	}
-	creds := credentials.NewTLS(&tls.Config{
+
+	/* load CA certificate file */
+	caCert, err := os.ReadFile(TLSCACertFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read CA cert: %w", err)
+	}
+	/* create a new certificate pool and append CA certificate */
+	certPool := x509.NewCertPool()
+	if ok := certPool.AppendCertsFromPEM(caCert); !ok {
+		return nil, fmt.Errorf("failed to add CA cert to pool")
+	}
+
+	/* build a TLS config */
+	tlsConfig := &tls.Config{
 		Certificates: []tls.Certificate{cert},
+		ClientCAs:    certPool,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
 		MinVersion:   tls.VersionTLS12,
-	})
-	return creds, nil
+	}
+
+	/* return TLS credentials */
+	return credentials.NewTLS(tlsConfig), nil
 }
